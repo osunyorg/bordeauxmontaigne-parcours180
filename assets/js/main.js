74ef8c124c063f20936a785f3f28f3d3869393ed
Matthieu Marchand <matthieumarchand.pro@gmail.com>
2026-04-29 23:48:14 +0200

    solution 1 amélioration de la qualité des images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 import "./theme/";
 
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('source').forEach(source => {
+        if (source.hasAttribute('srcset')) {
+            let url = source.getAttribute('srcset');
+            let nouvelleUrl = url.replace(/width=\d+/, 'width=1500');
+            source.setAttribute('srcset', nouvelleUrl);
+        }
+    });
+});
+
 // HOMEPAGE
 if (window.location.pathname === "/") {
     import("./home/grabToScroll");
