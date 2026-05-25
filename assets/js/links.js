581bc257ba2c5c7bd1aaf02fe112be1ea1862d51
Loic Leforestier <loicleforestier.pro@gmail.com>
2026-05-25 17:38:39 +0200

    added links

---
+++ assets/js/links.js
@@ -0,0 +1,11 @@
+const temoignages = document.querySelector(
+  ".block-class-decouvrir-des-videos-de-temoignages",
+);
+temoignages.style.scrollBehavior = "smooth";
+const outil = document.querySelector(
+  ".block-class-qu-est-ce-que-la-plateforme",
+);
+const utilisation = document.querySelector(".block-class-comment-l-utiliser");
+temoignages.id = "acceder-aux-videos-de-temoignages";
+outil.id = "un-outil-d-orientation";
+utilisation.id = "guide-d-utilisation";
